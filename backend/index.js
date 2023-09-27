a5674ca7710c184182036fd36f9dbcdb223cd1a8
Utpal Sharma <utpal@subhendu.io>
2023-09-27 18:01:56 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(bodyparser.json());
+app.use(cors());
 
 const userData = [
     {
@@ -18,7 +21,6 @@ const userData = [
     }
 ]
 
-app.use(bodyparser.json());
 
 const port = 3000
 
